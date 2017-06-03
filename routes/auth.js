6e26262f1a0b33642be9786d9b1dd332ef56f96b
hoto17296 <mail@hoto.me>
2017-06-03 18:02:24 +0900

    merge (GET|POST) /signup handler

--- routes/auth.js
+++ routes/auth.js
@@ -1,29 +1,27 @@
 const router = require('express').Router();
 const { Auth, AuthError } = require('../lib/auth');
 
-router.get('/signup', (req, res) => {
-  res.render('auth/signup', { title: 'Sign up', body: {}, errors: [] });
-});
-
-router.post('/signup', (req, res) => {
-  const { username, password1, password2 } = req.body;
+router.all('/signup', (req, res) => {
   const errors = [];
+  const body = req.body;
 
-  // Validation
-  if (password1 != password2) errors.push('Passwords don\'t match');
-
-  if (errors.length == 0) {
-    try {
-      Auth.register(username, password1);
-      return res.redirect('/');
+  if (req.method == 'POST') {
+    if (body.password1 != body.password2) {
+      errors.push('Passwords don\'t match');
     }
-    catch(e) {
-      if (e instanceof AuthError) errors.push(e.message);
-      else throw e;
+    else {
+      try {
+        Auth.register(body.username, body.password1);
+        return res.redirect('/');
+      }
+      catch(e) {
+        if (e instanceof AuthError) errors.push(e.message);
+        else throw e;
+      }
     }
   }
 
-  return res.render('auth/signup', { title: 'Sign up', body: req.body, errors });
+  return res.render('auth/signup', { title: 'Sign up', body, errors });
 });
 
 module.exports = router;
